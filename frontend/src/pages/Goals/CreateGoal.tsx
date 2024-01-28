0874ade6d4f45f75beeed4bfe46d0efde6f47521
Zehao Peng <pengzehao@Zehaos-MacBook-Pro.local>
2024-01-28 01:08:21 -0500

    added css styling to create gal

--- frontend/src/pages/Goals/CreateGoal.tsx
+++ frontend/src/pages/Goals/CreateGoal.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import './CreateGoal.css';
+import './CreateGoal.css'; // Make sure this is the correct path to your CSS file
 
 function CreateGoal() {
     const [goalName, setGoalName] = useState('');
@@ -13,18 +13,20 @@ function CreateGoal() {
                 placeholder="Name of the goal here"
                 className="goal-name-input"
             />
-            <input
-                type="text"
+
+            <textarea
                 value={goalName}
                 onChange={(e) => setGoalName(e.target.value)}
-                placeholder="Start typing and select the priority"
+                placeholder="Describe your goal"
+                className="goal-description-input"
             />
-            <div>
-                <button>High Priority</button>
-                <button>Medium Priority</button>
-                <button>Low Priority</button>
+
+            <div className="priority-input">
+                <button className="button high-priority" style={{color: '#D26060'}}>High Priority</button>
+                <button className="button medium-priority" style={{color: '#CC861E'}}>Medium Priority</button>
+                <button className="button low-priority" style={{color: '#7AA789'}}>Low Priority</button>
             </div>
-            <div>
+            <div className="visibility-toggle">
                 <label>
                     <input type="checkbox" />
                     Public
@@ -34,16 +36,18 @@ function CreateGoal() {
                     Private
                 </label>
             </div>
-            <div>
-                <button>+</button>
+            <div className="image-upload-section" style={{
+                    display: 'flex',         // Enables flexbox
+                    flexDirection: 'column', // Stack children vertically
+                    alignItems: 'center',    // Centers children horizontally in the flex container
+                    justifyContent: 'center' // Centers children vertically in the flex container
+                    }}>
+                <div>Drag & Drop or choose image to upload</div>
+                <input type="file"/>
             </div>
-            <div>
-                <div>Drag & Drop or choose file to upload</div>
-                <input type="file" />
-            </div>
-            <div>
-                <button>Cancel</button>
-                <button>Upload</button>
+            <div className="form-actions">
+                <button className="button cancel">Cancel</button>
+                <button className="button upload">Upload</button>
             </div>
         </div>
     );
