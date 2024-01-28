507a444cd5d0523a729a8999a0919f7d45bb6c6e
Isolumi <hubertx98@gmail.com>
2024-01-28 01:52:31 -0500

    update

--- frontend/src/pages/Goals/Goal.tsx
+++ frontend/src/pages/Goals/Goal.tsx
@@ -1,15 +1,31 @@
-import React from 'react'
-import { useParams } from 'react-router-dom'
+import { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
 import "../../Goal.css";
+import apiService from "../../services/apiService";
 
 function Goal() {
     let { id } = useParams();
+    const [goal, setGoal] = useState();
+
+    useEffect(() => {
+        const getProfile = async () => {
+            try {
+                const profile = await apiService.getProfile()
+                const goal = await apiService.GetGoal(profile['goals'][0])
+                setGoal(goal)
+            } catch (error) {
+                console.error('Error: ', error)
+            }
+        }
+
+        getProfile()
+    }, [])
+
     return (
-        <div className="title">
-            <h1>Goal {id}</h1>
+        <div className="container">
+            <h1 className="">Goal {id}</h1>
         </div>
     );
 }
 
 export default Goal;
-
